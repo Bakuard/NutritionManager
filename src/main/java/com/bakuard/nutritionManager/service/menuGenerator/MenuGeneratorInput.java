0e90e6c1da21fe99e26cc041175d3c5185a64f33
Bakuard <plainlist@gmail.com>
2022-06-07 12:07:59 +0600

    Задокументирован API модели генерации меню

--- src/main/java/com/bakuard/nutritionManager/service/menuGenerator/MenuGeneratorInput.java
+++ src/main/java/com/bakuard/nutritionManager/service/menuGenerator/MenuGeneratorInput.java
@@ -5,43 +5,96 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.Objects;
 
+/**
+ * Представляет собой набор ограничений используемый при подборе нового меню
+ * (подробнее см. {@link MenuGeneratorService#generate(MenuGeneratorInput)}).
+ */
 public class MenuGeneratorInput {
 
+    /**
+     * Ограничение на минимальное или максимальное кол-во продуктов одной конкретной категории.
+     * @param category категория продуктов.
+     * @param condition отношение "больше" или "меньше".
+     * @param quantity кол-во проудктов.
+     */
     public record ProductConstraint(String category, String condition, BigDecimal quantity) {}
 
+    /**
+     * Ограничение на минимальное или максимальное кол-во блюд с указанным тегом.
+     * @param dishTag тег блюда.
+     * @param condition отношение "больше" или "меньше".
+     * @param quantity кол-во блюда.
+     */
     public record DishConstraint(String dishTag, String condition, BigDecimal quantity) {}
 
     private String menuName;
     private BigDecimal maxPrice;
-    private BigDecimal servingNumber;
+    private int minMealsNumber;
+    private BigDecimal servingNumberPerMeal;
     private final List<ProductConstraint> productConstraints;
     private final List<DishConstraint> dishConstraints;
 
-    private MenuGeneratorInput() {
+    public MenuGeneratorInput() {
         productConstraints = new ArrayList<>();
         dishConstraints = new ArrayList<>();
     }
 
+    /**
+     * Устанавливает наименование для подбираемого меню.
+     * @param menuName наименование для подбираемого меню.
+     * @return ссылку на этот же объект.
+     */
     public MenuGeneratorInput setMenuName(String menuName) {
         this.menuName = menuName;
         return this;
     }
 
+    /**
+     * Устанавливает максимально допустую стоимость меню, которая расчитвается как суммарная стоимость всех
+     * продуктов необходимых для приготовелния блюд этого меню.
+     * @param maxPrice максимально допустимая стоимость меню.
+     * @return ссылку на этот же объект.
+     */
     public MenuGeneratorInput setMaxPrice(BigDecimal maxPrice) {
         this.maxPrice = maxPrice;
         return this;
     }
 
-    public MenuGeneratorInput setServingNumber(BigDecimal servingNumber) {
-        this.servingNumber = servingNumber;
+    /**
+     * Устанавливает минимальное кол-во приемов пищи на одного человека.
+     * @param minMealsNumber минимальное кол-во приемов пищи на одного человека.
+     * @return ссылку на этот же объект.
+     */
+    public MenuGeneratorInput setMinMealsNumber(int minMealsNumber) {
+        this.minMealsNumber = minMealsNumber;
         return this;
     }
 
+    /**
+     * Устанавливает кол-во порций в одном приеме пищи.
+     * @param servingNumberPerMeal кол-во порций в одном приеме пищи.
+     * @return ссылку на этот же объект.
+     */
+    public MenuGeneratorInput setServingNumberPerMeal(BigDecimal servingNumberPerMeal) {
+        this.servingNumberPerMeal = servingNumberPerMeal;
+        return this;
+    }
+
+    /**
+     * Добавляет ограничение на минимальное или максимальное кол-во продуктов определенной категории.
+     * Подробнее см. {@link ProductConstraint}.
+     * @return ссылку на этот же объект.
+     */
     public MenuGeneratorInput addProductConstraint(String category, String condition, BigDecimal quantity) {
         productConstraints.add(new ProductConstraint(category, condition, quantity));
         return this;
     }
 
+    /**
+     * Добавляет ограничение на минимальное или максимальное кол-во блюд с определенным тегом.
+     * Подробнее см. {@link DishConstraint}.
+     * @return ссылку на этот же объект.
+     */
     public MenuGeneratorInput addDishConstraint(String dishTag, String condition, BigDecimal quantity) {
         dishConstraints.add(new DishConstraint(dishTag, condition, quantity));
         return this;
@@ -55,8 +108,12 @@ public class MenuGeneratorInput {
         return maxPrice;
     }
 
-    public BigDecimal getServingNumber() {
-        return servingNumber;
+    public int getMinMealsNumber() {
+        return minMealsNumber;
+    }
+
+    public BigDecimal getServingNumberPerMeal() {
+        return servingNumberPerMeal;
     }
 
     public List<ProductConstraint> getProductConstraints() {
@@ -72,16 +129,18 @@ public class MenuGeneratorInput {
         if (this == o) return true;
         if (o == null || getClass() != o.getClass()) return false;
         MenuGeneratorInput that = (MenuGeneratorInput) o;
-        return Objects.equals(menuName, that.menuName) &&
+        return minMealsNumber == that.minMealsNumber &&
+                Objects.equals(menuName, that.menuName) &&
                 Objects.equals(maxPrice, that.maxPrice) &&
-                Objects.equals(servingNumber, that.servingNumber) &&
+                Objects.equals(servingNumberPerMeal, that.servingNumberPerMeal) &&
                 Objects.equals(productConstraints, that.productConstraints) &&
                 Objects.equals(dishConstraints, that.dishConstraints);
     }
 
     @Override
     public int hashCode() {
-        return Objects.hash(menuName, maxPrice, servingNumber, productConstraints, dishConstraints);
+        return Objects.hash(menuName, maxPrice, minMealsNumber, servingNumberPerMeal,
+                productConstraints, dishConstraints);
     }
 
     @Override
@@ -89,11 +148,11 @@ public class MenuGeneratorInput {
         return "MenuGeneratorInput{" +
                 "menuName='" + menuName + '\'' +
                 ", maxPrice=" + maxPrice +
-                ", servingNumber=" + servingNumber +
+                ", minMealsNumber=" + minMealsNumber +
+                ", servingNumberPerMeal=" + servingNumberPerMeal +
                 ", productConstraints=" + productConstraints +
                 ", dishConstraints=" + dishConstraints +
                 '}';
     }
-
 
 }
--- src/main/java/com/bakuard/nutritionManager/service/menuGenerator/MenuGeneratorService.java
+++ src/main/java/com/bakuard/nutritionManager/service/menuGenerator/MenuGeneratorService.java
@@ -1,6 +1,7 @@
 package com.bakuard.nutritionManager.service.menuGenerator;
 
 import com.bakuard.nutritionManager.model.Menu;
+import com.bakuard.nutritionManager.validation.*;
 
 public class MenuGeneratorService {
 
@@ -8,6 +9,53 @@ public class MenuGeneratorService {
 
     }
 
+    /**
+     * Подбирает новое меню. При подборе нового меню используется критерий минимальной суммарной стоимости
+     * продуктов необходимых для приготовления всех блюд этого меню. Входными данными для данного метода является
+     * набор ограничений передаваемых этому методу в виде объекта input.
+     * @param input набор ограничений для подбираемого меню (подробнее см. {@link MenuGeneratorInput}).
+     * @return новое меню.
+     * @throws ValidateException если выполняется хотя бы одно из следующих условий: <br/>
+     *         1. Метод {@link MenuGeneratorInput#getMenuName()} объекта input возвращает null. <br/>
+     *         2. Метод {@link MenuGeneratorInput#getMenuName()} объекта input возвращает наименование уже
+     *            существующего меню. <br/>
+     *         3. Метод {@link MenuGeneratorInput#getMaxPrice()} объекта input возвращает null. <br/>
+     *         4. Метод {@link MenuGeneratorInput#getMaxPrice()} объекта input возвращает отрицательное значение. <br/>
+     *         5. Метод {@link MenuGeneratorInput#getMinMealsNumber()} объекта input возвращает null. <br/>
+     *         5. Метод {@link MenuGeneratorInput#getMinMealsNumber()} объекта input возвращает ноль или
+     *            отрицательное значение. <br/>
+     *         6. Метод {@link MenuGeneratorInput#getServingNumberPerMeal()} объекта input возвращает null. <br/>
+     *         7. Метод {@link MenuGeneratorInput#getServingNumberPerMeal()} объекта input возвращает ноль или
+     *            отрицательное значение. <br/>
+     *         8. Если метод {@link MenuGeneratorInput.ProductConstraint#category()} одного из элементов списка
+     *            {@link MenuGeneratorInput#getProductConstraints()} возвращает null. <br/>
+     *         9. Если метод {@link MenuGeneratorInput.ProductConstraint#category()} одного из элементов списка
+     *            {@link MenuGeneratorInput#getProductConstraints()} возвращает несуществующую категорию
+     *            продуктов. <br/>
+     *         10. Если метод {@link MenuGeneratorInput.ProductConstraint#condition()} одного из элементов списка
+     *            {@link MenuGeneratorInput#getProductConstraints()} возвращает null. <br/>
+     *         11. Если метод {@link MenuGeneratorInput.ProductConstraint#condition()} одного из элементов списка
+     *            {@link MenuGeneratorInput#getProductConstraints()} возвращает значение не принадлежащее
+     *            множеству {"lessOrEqual", "greaterOrEqual"}. <br/>
+     *         12. Если метод {@link MenuGeneratorInput.ProductConstraint#quantity()} одного из элементов списка
+     *            {@link MenuGeneratorInput#getProductConstraints()} возвращает null. <br/>
+     *         13. Если метод {@link MenuGeneratorInput.ProductConstraint#quantity()} одного из элементов списка
+     *            {@link MenuGeneratorInput#getProductConstraints()} возвращает отрицательное значение. <br/>
+     *         14. Если метод {@link MenuGeneratorInput.DishConstraint#dishTag()} одного из элементов списка
+     *            {@link MenuGeneratorInput#getDishConstraints()} возвращает null. <br/>
+     *         15. Если метод {@link MenuGeneratorInput.DishConstraint#dishTag()} одного из элементов списка
+     *            {@link MenuGeneratorInput#getDishConstraints()} возвращает несуществующий тег. <br/>
+     *         16. Если метод {@link MenuGeneratorInput.DishConstraint#condition()} одного из элементов списка
+     *            {@link MenuGeneratorInput#getDishConstraints()} возвращает null. <br/>
+     *         17. Если метод {@link MenuGeneratorInput.DishConstraint#condition()} одного из элементов списка
+     *            {@link MenuGeneratorInput#getDishConstraints()} возвращает значение не принадлежащее
+     *            множеству {"lessOrEqual", "greaterOrEqual"}. <br/>
+     *         18. Если метод {@link MenuGeneratorInput.DishConstraint#quantity()} одного из элементов списка
+     *            {@link MenuGeneratorInput#getDishConstraints()} возвращает null. <br/>
+     *         19. Если метод {@link MenuGeneratorInput.DishConstraint#quantity()} одного из элементов списка
+     *            {@link MenuGeneratorInput#getDishConstraints()} возвращает отрицательное значение. <br/>
+     *         20. Если невозможно подобрать меню с заданными ограничениями. <br/>
+     */
     public Menu generate(MenuGeneratorInput input) {
         return null;
     }
